0fcdc15b846ceafd3cbe5a916b3df40667505b42
Fantasy Renald Zhafar <60190893+LizRelDev@users.noreply.github.com>
2024-09-16 13:25:29 +0700

    Update qassa_onclite.mk

--- qassa_onclite.mk
+++ qassa_onclite.mk
@@ -34,6 +34,6 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="walleye-user 8.1.0 OPM1.171019.011 4448085 release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-    BUILD_FINGERPRINT := "google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys"
+BUILD_FINGERPRINT := "google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys"
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
